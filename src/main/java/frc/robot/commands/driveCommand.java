dd799dd4b83465181cd5462fbfe1e2b4b8ac3937
Shane Wallace <colinwallace924@gmail.com>
2022-01-24 16:05:04 -0500

    files renamed

--- src/main/java/frc/robot/commands/driveCommand.java
+++ src/main/java/frc/robot/commands/driveCommand.java
@@ -4,21 +4,17 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.driveSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
+public class driveCommand extends CommandBase {
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public driveCommand(driveSubsystem subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
--- src/main/java/frc/robot/subsystems/driveSubsystem.java
+++ src/main/java/frc/robot/subsystems/driveSubsystem.java
@@ -6,9 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class driveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public driveSubsystem() {}
 
   @Override
   public void periodic() {
